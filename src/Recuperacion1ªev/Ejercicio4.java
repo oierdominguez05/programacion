090881ffd90cd869ad6a8947ab3604187af04617
Oier Dominguez <oierdominguez05@gmail.com>
2025-12-28 10:28:06 +0100

    Ej4 recuperacion

--- src/Recuperacion1ªev/Ejercicio4.java
+++ src/Recuperacion1ªev/Ejercicio4.java
@@ -26,17 +26,80 @@ public class Ejercicio4 {
 
 		String numero1 = numero + "";
 
-		for (int i = 0; i < numero1.length(); i++) {
-
-			String resultado = numero1.substring(i);
-			if (i == 1) {
-				System.out.println("Aparece el 1");
-
+		
+			if (numero1.contains("0")) {
+				System.out.println("Aparece el 0");
 			}
-			if (i == 2) {
+			if (numero1.contains("1")) {
+				System.out.println("Aparece el 1");
+			}
+			if (numero1.contains("2")) {
 				System.out.println("Aparece el 2");
 			}
-		}
+		    if (numero1.contains("3")) {
+				System.out.println("Aparece el 3");
+			}
+		    if (numero1.contains("4")) {
+				System.out.println("Aparece el 4");
+			}
+		    if (numero1.contains("5")) {
+				System.out.println("Aparece el 5");
+			}
+		    if (numero1.contains("6")) {
+				System.out.println("Aparece el 6");
+			}
+		    if (numero1.contains("7")) {
+				System.out.println("Aparece el 7");
+			}
+		    if (numero1.contains("8")) {
+				System.out.println("Aparece el 8");
+			}
+		    if (numero1.contains("9")) {
+				System.out.println("Aparece el 9");
+			}
+			
+		    
+		    
+		    
+		    
+		    
+		    
+		    if (!numero1.contains("0")) {
+				System.out.println("NO aparece el 0");
+			}
+			if (!numero1.contains("1")) {
+				System.out.println("NO aparece el 1");
+			}
+			if (!numero1.contains("2")) {
+				System.out.println("NO aparece el 2");
+			}
+		    if (!numero1.contains("3")) {
+				System.out.println("NO aparece el 3");
+			}
+		    if (!numero1.contains("4")) {
+				System.out.println("NO aparece el 4");
+			}
+		    if (!numero1.contains("5")) {
+				System.out.println("NO aparece el 5");
+			}
+		    if (!numero1.contains("6")) {
+				System.out.println("NO aparece el 6");
+			}
+		    if (!numero1.contains("7")) {
+				System.out.println("NO aparece el 7");
+			}
+		    if (!numero1.contains("8")) {
+				System.out.println("NO aparece el 8");
+			}
+		    if (!numero1.contains("9")) {
+				System.out.println("NO aparece el 9");
+			}
+		    
+		    
+		    
+		    
+			
+		
 
 	}
 
